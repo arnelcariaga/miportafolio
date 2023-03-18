c1bccd1854156afe365397ae5783f3312743c13e
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2023-03-18 11:43:19 -0400

    Remove comment and title

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,6 @@ import ProjectsList from "@/components/ProjectsList";
 export default function Home() {
   return (
     <HomeLayout>
-      {/* Projects list */}
-      <h1>Mis proyectos</h1>
-
       <ProjectsList />
     </HomeLayout>
   );
