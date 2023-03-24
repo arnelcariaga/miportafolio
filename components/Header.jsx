913a1d8ec082edf40bc8c1a25e8e9ed659288400
Arnel Cariaga <42476817+arnelcariaga@users.noreply.github.com>
2023-03-23 22:47:24 -0400

    Change some styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 function Header({ devTxtRef }) {
     return (
-        <div className="text-center">
-            <>
+        <div className="vh-100 d-flex align-items-center text-center">
+            <div>
                 <h1 className="display-1 fw-bold">Hola,</h1>
                 <span>
                     <h1 className="display-1 fw-bold">Mi nombre es Arnel Cariaga, Soy <span className="dev-txt text-info" ref={devTxtRef}>Programador</span>
@@ -9,11 +9,10 @@ function Header({ devTxtRef }) {
                         <span className='web-txt d-inline-block'>Web</span> & <span className='mobile-txt d-inline-block'>Mobile.</span>
                     </h1>
                 </span>
-            </>
-
-            <figcaption className="blockquote-footer pt-5 w-50 mx-auto">
+                <figcaption className="blockquote-footer pt-5 w-50 mx-auto">
                 <span className='fs-5'>Aplicaciones mobiles y web con la mas alta calidad y diseño que se adapta a tus necesidades.</span>
             </figcaption>
+            </div>
         </div>
     )
 }
